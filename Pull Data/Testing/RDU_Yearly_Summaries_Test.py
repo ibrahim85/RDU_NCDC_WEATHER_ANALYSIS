afc7e4168f372b76098b2dea46551b96b6ed02f6
kramer003 <andrew.kramer526@gmail.com>
2016-10-25 22:17:49 -0400

    Complete Loop for Yearly Data
    Loop for RDU yearly data 1946-present

--- Pull Data/Testing/RDU_Yearly_Summaries_Test.py
+++ Pull Data/Testing/RDU_Yearly_Summaries_Test.py
@@ -36,3 +36,36 @@ results=pd.DataFrame({
 	})
 
 print(results)
+
+
+
+
+#Put in a function to be used to collect all data
+#url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY&stationid=GHCND:USW00013722&startdate=2006-01-01&enddate=2016-01-01&limit=100&datatypeid=PRCP&datatypeid=TAVG' #Avg Temp and Precipitation per month at rdu
+
+def return_results(url):
+
+	headers = {'token': 'dKuJxEKlhIJuoZSjvKULivIPXWsRqspt' }
+
+	#Execute call and parse response
+	response = requests.get(url, headers=headers)
+	parsed=json.loads(response.text)
+
+	year=[]
+	PRCP=[]
+	TAVG=[]
+
+	for record in parsed['results']:
+		
+		if record['datatype']=='PRCP':
+			year.append(record['date'])
+			PRCP.append(record['value'])
+
+		else:
+			TAVG.append(record['value'])
+
+	return year, PRCP, TAVG
+
+#return_results(url)
+
+
--- Pull Data/Testing/RDU_Yearly_loop.py
+++ Pull Data/Testing/RDU_Yearly_loop.py
@@ -11,16 +11,25 @@ f=np.arange(1946,2017,10)
 
 
 def year_loop(start,end, by):
-	f=np.arange(start, end+1, by)
-    
-	for year in f:
-		print(year)
-	
-		
 
+	url='http://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY&stationid=GHCND:USW00013722&startdate=1946-01-01&enddate=1956-01-01&limit=100&datatypeid=PRCP&datatypeid=TAVG'
+
+	ranges=np.arange(start, end+1, by)
+    
+	for i in range(len(ranges)-1):
+		print(i)
+		if i==6:
+			break
+		else:
+			url=url.replace(str(ranges[i+1]),str(ranges[i+2]))
+			url=url.replace(str(ranges[i]),str(ranges[i+1]))
+			print(url)
+	
 print(year_loop(1946, 2017, 10))
 
 
-url='http://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY&stationid=GHCND:USW00013722&startdate=2006-01-01&enddate=2016-01-01&limit=100&datatypeid=PRCP&datatypeid=TAVG'
-url2=url.replace(str(2006), str(2060))
-print(url2)
+#url='http://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY&stationid=GHCND:USW00013722&startdate=2006-01-01&enddate=2016-01-01&limit=100&datatypeid=PRCP&datatypeid=TAVG'
+#url2=url.replace(str(2006), str(2060))
+#print(url2)
+for i in range(len(f)):
+	print(i)
